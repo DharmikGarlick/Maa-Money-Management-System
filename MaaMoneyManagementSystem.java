a8d7250effd74cbc97e2e41d1e89bb79e88be0dc
DharmikGarlick <dharmik.garlick@gmail.com>
2025-06-03 17:01:23 +0600

    Update MaaMoneyManagementSystem.java
    Commented out code for viewing all forms had usage of old class name.
    'GroceryManagementForm(...)...' was changed to 'MainForm(...)...'

--- MaaMoneyManagementSystem.java
+++ MaaMoneyManagementSystem.java
@@ -2,7 +2,7 @@ public class MaaMoneyManagementSystem {
     public static void main(String[] args) {
         new LoginForm().setVisible(true);
         //Remove these comments to see all the forms in one window
-        // new GroceryManagementForm(new Customer(null, null, null)).setVisible(true);
+        // new MainForm(new Customer(null, null, null)).setVisible(true);
         // new ViewForm(new Customer(null, null, null)).setVisible(true);
         // new SimpleViewForm(new Customer(null, null, null)).setVisible(true);
         // new ReviewForm().setVisible(true);
